195b7c8d7b843e3c29fdacd7101b45f5a9b121a2
Rejaur Rahman <rejau@mac.mynetworksettings.com>
2026-02-07 23:37:28 -0500

    Tailwind setup + UI polish + fixes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,8 @@
 module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/components/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 };
